a3210d2c434526b99082504aa7bfe8b100f88942
dominicsacco1 <58260209+dominicsacco1@users.noreply.github.com>
2019-11-28 17:40:47 +0200

    changed files commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { CheckboxesComponent } from './checkboxes/checkboxes.component';
+import { FormsModule } from '@angular/forms';
+import { LOCALE_ID } from '@angular/core';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'en' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/checkboxes/checkboxes.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkboxes',
+  templateUrl: './checkboxes.component.html',
+  styleUrls: ['./checkboxes.component.scss']
+})
+export class CheckboxesComponent {
+
+  name = 'Angular';
+
+}
